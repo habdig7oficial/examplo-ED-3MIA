53393dae1b377b5decb3badf7db656bac08c51fd
Mateus Vieira <habdig7@gmail.com>
2023-09-19 10:01:54 -0300

    testo generico

--- src/components/Commit-teste.tsx
+++ src/components/Commit-teste.tsx
@@ -3,6 +3,7 @@ export default function(){
         <>
             <img src="/Images/richard-stallman.jpeg" />
             <p>Hello From Teste</p>
+            <p>O Home Mais Livre do Mundo Hoje kkk</p>
         </>
     )
 }
